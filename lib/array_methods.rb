dbbe92fc2fc6ea1d601967f882c4ef1b8725604d
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-12 09:02:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,13 +17,15 @@ def using_reverse(array)
 end
 
 def using_first(array)
-
+  mycolors = ["green","red", "white"]
+  mycolors.first
 end
 
 def using_last(array)
-
+  mycolors = ["green","red", "white"]
+  mycolors.last
 end
 
 def using_size(array)
-
+mycolors = ["green","red", "white"]
 end
